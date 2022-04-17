0f3b975489eb2a37a200da17822f88e614c18553
Shisir <Sisirkhanal20@gmail.com>
2022-04-17 13:06:45 +0545

    initial

--- snake/game.js
+++ snake/game.js
@@ -1,15 +1,26 @@
-import { snakeSpeed } from "./snake.js";
+import {
+  update as updateSnake,
+  draw as drawSnake,
+  snakeSpeed,
+} from "./snake.js";
 let lastRenderTime = 0;
+const gameBoard =
+  document.getElementById("game-board");
 function main(currentime) {
   window.requestAnimationFrame(main);
   const secondsSinceLastRender =
     (currentime - lastRenderTime) / 1000;
   if (secondsSinceLastRender < 1 / snakeSpeed)
     return;
-  console.log("render");
+  // console.log("render");
   lastRenderTime = currentime;
   update();
   draw();
 }
 window.requestAnimationFrame(main);
-function update() {}
+function update() {
+  updateSnake();
+}
+function draw() {
+  drawSnake(gameBoard);
+}
